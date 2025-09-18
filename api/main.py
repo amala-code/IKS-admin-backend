aed6a748f630458cce7dab6340d237e17cd8048b
amala-code <amala.sebastian@47billion.com>
2025-09-19 00:48:16 +0530

    last attempt 1

--- api/main.py
+++ api/main.py
@@ -35,7 +35,7 @@ async def ping():
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8001, reload=True)
 
 
 
